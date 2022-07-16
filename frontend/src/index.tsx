16b72a8ed4083198534a2c054b4ba3ba5d883f05
Jose Javier <jose.javier@pucp.edu.pe>
2022-07-16 13:47:22 -0500

    feat(frontend): add tailwind and set app layout

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import './index.scss';
-import App from './components/App';
-import reportWebVitals from './reportWebVitals';
-import ReactDOM from 'react-dom/client';
+import React from "react";
+import "./index.css";
+import App from "./components/App";
+import reportWebVitals from "./reportWebVitals";
+import ReactDOM from "react-dom/client";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App/>
-  </React.StrictMode>,
+    <App />
+  </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
